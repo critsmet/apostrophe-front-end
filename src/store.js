62bd0a4d25d692de1150a13e3ce5ff53ac616611
critsmet <36425627+critsmet@users.noreply.github.com>
2018-09-12 15:16:39 -0400

    Removed dev middleware

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 import createHistory from 'history/createBrowserHistory'
 import rootReducer from './rootReducer'
@@ -8,22 +7,15 @@ import rootReducer from './rootReducer'
 export const history = createHistory()
 
 const initialState = {}
-const enhancers = []
 const middleware = [
   thunk,
   routerMiddleware(history)
 ]
 
-
-const composedEnhancers = composeWithDevTools(
-  applyMiddleware(...middleware),
-  ...enhancers
-)
-
 const store = createStore(
   connectRouter(history)(rootReducer),
   initialState,
-  composedEnhancers
+  applyMiddleware(...middleware)
 )
 
 export default store
