92a1b74dc1cee5ca96f55cbd46abc4f7d4755d78
critsmet <36425627+critsmet@users.noreply.github.com>
2018-09-18 14:52:33 -0400

    Add underline

--- src/components/app/Info.js
+++ src/components/app/Info.js
@@ -13,7 +13,7 @@ const Info = () => {
           this website was born out of an appreciation for the artistry and unique voices housed in online publications.
           despite how pervasive they are in contemporary culture, they lack the legitimate feeling of newspapers and print publications—perhaps because of the lack of an index to reference them.
           the purpose of apostrophe is two-fold: to allow readers of digital publications to discover new ones by providing a well-compiled source, and to give writers a tool for finding new platforms where they can share their work.
-          if you would like to contribute to the project, please send an e-mail to apostropheonline@gmail.com with information about a publication you think belongs here.
+          if you would like to contribute to the project, please send an e-mail to <u>apostropheonline@gmail.com</u> with information about a publication you think belongs here.
           apostrophe shares publications that are primarily digital, or have a digital version of a print publication that provides online access to most or all of the same material.
           this is the first iteration of apostrophe and new ideas are already being sketched.
           feel free to send a message if you are interested in getting involved with the project.
