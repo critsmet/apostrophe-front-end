b53dd74b5a9de0a821ff63eea6a50c96f6147e32
Chris Metzger <chrismetzgr@gmail.com>
2018-12-18 16:11:31 -0500

    update info page

--- src/components/app/Info.js
+++ src/components/app/Info.js
@@ -12,7 +12,7 @@ const Info = () => {
       <div className="flex mt4-ns mt3 w-100 w-30-l w-40-m ml3">
         <span className="info">
           apostrophe was created out of an appreciation for the artistry and unique voices housed in digital publications.
-          despite how pervasive they are in contemporary culture, they remain undervalued content medium, perhaps because of the lack of an index to reference them.
+          despite how pervasive they are in contemporary culture, they remain an undervalued medium for content, perhaps because of the lack of an index to reference them.
           the purpose of apostrophe is two-fold: to allow readers of digital publications to discover new ones by providing a well-compiled source, and to give writers a tool for finding new platforms where they can share their work.
           if you would like to contribute to the project, please send an e-mail to <u>apostropheonline@gmail.com</u> with information about a publication you think belongs here.
           apostrophe shares publications that are primarily digital, or have a digital version of a print publication that provides online access to most or all of the same material.
