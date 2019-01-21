552521eafaa7047d841a2b6db242e36128265258
critsmet <36425627+critsmet@users.noreply.github.com>
2019-01-20 22:56:46 -0500

    More directory changes, this time in the rootReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
-import appReducer from './components/app/appMod'
-import browseReducer from './components/browse/browseMod'
-import navReducer from './components/nav/navMod'
-import pubReducer from './components/pubs/pubMod'
-import userReducer from './components/user/userMod'
-import userFormReducer from './components/userForm/userFormMod'
+import appReducer from '../components/app/appMod'
+import browseReducer from '../components/browse/browseMod'
+import navReducer from '../components/nav/navMod'
+import pubReducer from '../components/pubs/pubMod'
+import userReducer from '../components/user/userMod'
+import userFormReducer from '../components/userForm/userFormMod'
 
 export default combineReducers({
   app: appReducer,
