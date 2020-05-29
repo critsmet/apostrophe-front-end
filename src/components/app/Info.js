f2ca7caede2bb95aae258f8280a23b3c0d0b6a59
critsmet <36425627+critsmet@users.noreply.github.com>
2020-05-28 22:55:12 -0500

    update info section

--- src/components/app/Info.js
+++ src/components/app/Info.js
@@ -14,13 +14,10 @@ const Info = () => {
           apostrophe was created out of an appreciation for the artistry and unique voices housed in digital publications.
           despite how pervasive they are in contemporary culture, they remain an undervalued medium for content, perhaps because of the lack of an index to reference them.
           the purpose of apostrophe is two-fold: to allow readers of digital publications to discover new ones by providing a well-compiled source, and to give writers a tool for finding new platforms where they can share their work.
-          if you would like to contribute to the project, please send an e-mail to <u>apostropheonline@gmail.com</u> with information about a publication you think belongs here.
+          if you would like to contribute to the project, please send a message to the creator, <u><a href="http://critsmet.github.io/bio">chris metzger</a></u>, with information about a publication you think belongs here.
           apostrophe shares publications that are primarily digital, or have a digital version of a print publication that provides online access to most or all of the same material.
           this is the first iteration of apostrophe and new ideas are already being sketched.
           feel free to send a message if you are interested in getting involved with the project.
-          <br/>
-          <br/>
-          apostrophe was created by <u><a href="http://chrismetzger.site">chris metzger</a></u>
         </span>
       </div>
     </div>
